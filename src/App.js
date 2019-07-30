97a6e920692e971a129b1a67bd167aebc8a689b7
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-29 22:43:24 -0400

    board component done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
 import "./App";
 import "./index";
-import Card from "./components/card/index.js";
+import Board from "./components/board/index";
 
 function App() {
   const [flipped, setFlipped] = useState([]);
 
-  const handleClick = id => setFlipped(id);
+  const handleClick = id => setFlipped([...flipped, id]);
 
   return (
     <div>
       <h1>Memory Game</h1>
       <h2>Match the cards!</h2>
-      <Card
-        id={1}
-        width={100}
-        height={100}
-        back={`/public/img/CardBack.png`}
-        front={"/public/img/Bat.png"}
-        flipped={flipped.includes(1)}
-        handleClick={() => handleClick(1)}
-      />
     </div>
   );
 }
---
+++ src/components/board/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Proptypes from "prop-types";
+import Card from "../card";
+
+export default function Board({ cards, flipped, handleClick }) {
+  return (
+    <div className="board">
+      {cards.map(card => (
+        <Card
+          key={card.id}
+          type={card.type}
+          id={card.id}
+          width={card.width}
+          height={card.height}
+          flipped={flipped.includes(card.id)}
+          handleClick={() => handleClick(card.id)}
+        />
+      ))}
+    </div>
+  );
+}
+
+Board.propTypes = {
+  cards: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
+  flipped: Proptypes.arrayOf(Proptypes.number).isRequired,
+  handleClick: Proptypes.func.isRequired
+};
