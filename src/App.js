6791cdece914d5a54da54c5f0b95cb95eb2c1701
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-29 22:18:06 -0400

    card show up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
-import "./App.css";
-import Card './components/card'
-
+import React, { useState } from "react";
+import "./App";
+import "./index";
+import Card from "./components/card/index.js";
 
 function App() {
+  const [flipped, setFlipped] = useState([]);
+
+  const handleClick = id => setFlipped(id);
+
   return (
     <div>
       <h1>Memory Game</h1>
@@ -15,7 +19,7 @@ function App() {
         back={`/public/img/CardBack.png`}
         front={"/public/img/Bat.png"}
         flipped={flipped.includes(1)}
-        handlerClick={() => handlerClick(1)}
+        handleClick={() => handleClick(1)}
       />
     </div>
   );
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./styles";
+import "./styles.css";
 
 export default function Card({
   handleClick,
