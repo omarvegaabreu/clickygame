2be6f243271f3b97530f5e604afc6a7202649cd8
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-30 13:54:09 -0400

    board is responsive

--- src/App.js
+++ src/App.js
@@ -7,18 +7,40 @@ import initializeDeck from "./components/deck/deck";
 function App() {
   const [flipped, setFlipped] = useState([]);
   const [cards, setCards] = useState([]);
+  const [dimesion, setDimension] = useState(400);
 
   useEffect(() => {
+    resizeBoard();
     setCards(initializeDeck());
   }, []);
 
+  useEffect(() => {
+    const resizeListener = window.addEventListener("resize", resizeBoard);
+
+    return () => window.removeEventListener("resize", resizeListener);
+  });
+
   const handleClick = id => setFlipped([...flipped, id]);
+
+  const resizeBoard = () => {
+    setDimension(
+      Math.min(
+        document.documentElement.clientWidth,
+        document.documentElement.clientHeight
+      )
+    );
+  };
 
   return (
     <div>
       <h1>Memory Game</h1>
       <h2>Match the cards!</h2>
-      <Board cards={cards} flipped={flipped} handleClick={handleClick} />
+      <Board
+        dimension={dimesion}
+        cards={cards}
+        flipped={flipped}
+        handleClick={handleClick}
+      />
     </div>
   );
 }
--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Proptypes from "prop-types";
 import Card from "../card";
+import "./styles.css";
 
-export default function Board({ cards, flipped, handleClick }) {
+export default function Board({ dimension, cards, flipped, handleClick }) {
   return (
     <div className="board">
       {cards.map(card => (
@@ -10,8 +11,8 @@ export default function Board({ cards, flipped, handleClick }) {
           key={card.id}
           type={card.type}
           id={card.id}
-          width={100}
-          height={100}
+          width={dimension / 4.5}
+          height={dimension / 4.5}
           flipped={flipped.includes(card.id)}
           handleClick={() => handleClick(card.id)}
         />
@@ -21,6 +22,7 @@ export default function Board({ cards, flipped, handleClick }) {
 }
 
 Board.propTypes = {
+  dimension: Proptypes.number.isRequired,
   cards: Proptypes.arrayOf(Proptypes.shape({})).isRequired,
   flipped: Proptypes.arrayOf(Proptypes.number).isRequired,
   handleClick: Proptypes.func.isRequired
